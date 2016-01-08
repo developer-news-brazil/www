93e8763d4c39a49ae73242f5e4707c6221c16e71
Talysson <talyssonoc@gmail.com>
2016-01-08 01:34:04 -0200

    feat(gulp): Add paths file

--- tasks/js-lint.js
+++ tasks/js-lint.js
@@ -2,8 +2,10 @@ import gulp from 'gulp';
 
 import eslint from 'gulp-eslint';
 
+import paths from './paths';
+
 export default () => {
-  return gulp.src(['app/**/*.js', '!node_modules/**'])
+  return gulp.src([paths.js.glob])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
---
+++ tasks/paths.js
@@ -0,0 +1,6 @@
+export default {
+  js: {
+    src: 'app',
+    glob: 'app/**/*.js'
+  }
+};
