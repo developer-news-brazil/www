4ce96916ff4d8941944cc8738c41103046602987
Fernando Fleury <me@fleury.io>
2016-01-28 12:50:42 -0200

    feat(front-tests): add mocha to .eslintrc

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,19 @@
+import HomeHandler from 'app/components/home';
+import AboutHandler from 'app/components/about';
+
 export default {
   home: {
     path: '/',
     method: 'get',
     page: 'home',
     title: 'Home',
-    handler: require('app/components/Home')
+    handler: HomeHandler
   },
   about: {
     path: '/about',
     method: 'get',
     page: 'about',
     title: 'About',
-    handler: require('app/components/About')
+    handler: AboutHandler
   }
 };
